fdd2fe77dd879d398be901b6f111a2a7a8d9811c
Renan C Soares <renan-csoares@hotmail.com>
2026-03-10 22:39:17 +0000

    feat: add web manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
+
+const manifest = {
+    name: 'Pre-renewal RO MVP Tracking',
+    short_name: 'MVP Tracker',
+    description: 'Track MVP respawn timers for Pre-renewal Ragnarok Online',
+    start_url: '/mvp-tracking/',
+    scope: '/mvp-tracking/',
+    background_color: '#000000',
+    theme_color: '#ffca16',
+    icons: [
+        {
+            src: '/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+        },
+        {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+        },
+        {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+        },
+    ],
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +38,13 @@ export default defineConfig({
         react({
             babel: {
                 plugins: ['babel-plugin-react-compiler'],
+            },
+        }),
+        VitePWA({
+            manifest,
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['mvps/*.png'],
             },
         }),
     ],
